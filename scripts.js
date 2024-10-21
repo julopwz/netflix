2eddc94188ce4db42922b0c958451d3dc3f9c1a1
Julia Lopes <julialopes@usp.br>
2024-10-20 23:58:03 -0300

    criando modal js

--- scripts.js
+++ scripts.js
@@ -2,10 +2,21 @@
 
 let botaoSom = document.querySelector(".som")
 let video = document.querySelector(".video")
+let botao = document.querySelector(".link-info")
+let modal = document.querySelector(".modal")
 
+// ligar o som
 
 botaoSom.addEventListener("click", ligaSom)
 
 function ligaSom(){
     video.muted = false
+}
+
+// mostrar modal
+
+botao.addEventListener("click", mostrarModal)
+
+function mostrarModal(){
+    modal.style.display = "block"
 }
